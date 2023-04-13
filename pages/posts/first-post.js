230aefae802ab04654ae8d5cf7c65881a614394e
Lucas Carlos <lucas2carlos@gmail.com>
2023-04-13 13:59:45 -0400

    update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
 export default function firstPost(){
-    return <h1>First Post</h1>
+    return(
+        <>
+        <h1>First Post</h1>
+        <h2><Link href="/">Back to home</Link></h2>
+        </>
+    )
 }
