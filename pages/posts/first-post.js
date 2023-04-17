e6452c479fcea554c6c78c287cc9e4cdf1edd723
Lucas Carlos <lucas2carlos@gmail.com>
2023-04-17 14:49:56 -0400

    importing Layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import Head from "next/head"
 import Script from "next/script"
+import Layout from "../../components/layout"
 
 export default function firstPost(){
     return(
-        <>
+        <Layout>
         <Head>
             <title>First Post</title>
             <Script
@@ -15,6 +16,6 @@ export default function firstPost(){
         </Head>
         <h1>First Post</h1>
         <h2><Link href="/">Back to home</Link></h2>
-        </>
+        </Layout>
     )
 }
